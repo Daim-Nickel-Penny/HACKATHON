5f2c356cddb74a9c3f325406defb1a53f81711a0
1804449@kiit.ac.in <1804449@kiit.ac.in>
2020-08-17 00:37:25 +0530

    Get using xml

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import Val from './Val';
 import './App.css';
 
 function App() {
+ 
+
   return (
     <div className="App">
+    
       <Val/>
     </div>
   );
